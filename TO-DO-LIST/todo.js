45cb4671c0f9e6b725c05ea02799d39d7738d2f2
M-Muthu-Pandi <muthupandim5799@gmail.com>
2024-06-30 16:42:33 +0530

    To-do file updated

--- TO-DO-LIST/todo.js
+++ TO-DO-LIST/todo.js
@@ -1,24 +1,30 @@
 const taskInput = document.getElementById('task-input');
-const addTaskBtn = document.getElementById('add-task-btn');
 const taskList = document.getElementById('task-list');
-
-addTaskBtn.addEventListener('click', addTask);
+const taskFinishList = document.getElementById('task-finish-list');
 
 function addTask() {
-	const taskText = taskInput.value;
+	const taskText = taskInput.value.trim();
     
-	if (taskText) {
-		const taskListItem = document.createElement('li');
-		taskListItem.textContent = taskText;
-		taskList.appendChild(taskListItem);
+	if (taskText === '') {
+		alert("You must write something");
+	}
+	else {
+		const btn1 = '<div><button onclick="finishItem(event)"><i class="fa-solid fa-check"></i></button><button onclick="deleteItem(event)"><i class="fa-solid fa-trash"></i></button></div>';
+		const taskContent = '<div class="mu2-content">'+ taskText + '</div>';
+		const li = document.createElement('li');
+		li.innerHTML = taskContent + btn1;
+		taskList.appendChild(li);
 		taskInput.value = '';
 	}
 }
 
-taskList.addEventListener('click', toggleTaskDone);
+function deleteItem(event) {
+	event.target.parentElement.parentElement.parentElement.remove();
+}
 
-function toggleTaskDone(event) {
-	if (event.target.tagName === 'LI') {
-		event.target.classList.toggle('done');
+function finishItem(event) {
+	if(event.target.tagName === "BUTTON") {
+		const taskFinish = event.target.parentNode;
+		taskFinishList.appendChild(taskFinish);
 	}
 }
